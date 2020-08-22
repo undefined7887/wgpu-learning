4849ba3ff2cf9fd7ea221be54926dc48dcecc13a
Ярослав <undefined7887@gmail.com>
2020-08-22 03:09:28 +0300

    Implemented simple app lifecycle & first render pass!

---
+++ src/main.rs
@@ -0,0 +1,195 @@
+use winit::{
+    event::*,
+    event_loop::{ControlFlow, EventLoop},
+    window::{Window, WindowBuilder},
+};
+use wgpu::Limits;
+use futures::executor::block_on;
+
+fn main() {
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .build(&event_loop)
+        .expect("Failed to build window");
+
+    let mut state = block_on(State::new(&window));
+
+    event_loop.run(move |event, _, control_flow| {
+        match event {
+            Event::RedrawRequested(_) => {
+                println!("Redraw requested");
+                state.update();
+                state.render();
+            }
+
+            Event::MainEventsCleared => {
+                //window.request_redraw();
+            }
+
+            // Simple event for window
+            Event::WindowEvent {
+                ref event,
+                window_id
+            } if window.id() == window_id => {
+                // Firstly sending event to state
+                if state.input(event) {
+                    return;
+                }
+
+                // Secondly handling event here
+                match event {
+                    // When window closes
+                    WindowEvent::CloseRequested => {
+                        *control_flow = ControlFlow::Exit
+                    }
+
+                    // When user press keyboard button
+                    WindowEvent::KeyboardInput {
+                        input,
+                        ..
+                    } => {
+                        // Closing window if user pressed escape
+                        if let KeyboardInput {
+                            state: ElementState::Pressed,
+                            virtual_keycode: Some(VirtualKeyCode::Escape),
+                            ..
+                        } = input {
+                            *control_flow = ControlFlow::Exit
+                        }
+                    }
+
+                    // When window is resized
+                    WindowEvent::Resized(new_size) => {
+                        state.resize(*new_size);
+                    }
+
+                    // When user changed display resolution
+                    WindowEvent::ScaleFactorChanged {
+                        new_inner_size,
+                        ..
+                    } => {
+                        state.resize(**new_inner_size);
+                    }
+                    _ => {}
+                }
+            }
+            _ => {}
+        }
+    })
+}
+
+struct State {
+    surface: wgpu::Surface,
+    adapter: wgpu::Adapter,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    swap_chain: wgpu::SwapChain,
+    swap_chain_desc: wgpu::SwapChainDescriptor,
+    size: winit::dpi::PhysicalSize<u32>,
+}
+
+impl State {
+    async fn new(window: &Window) -> Self {
+        let size = window.inner_size();
+        let surface = wgpu::Surface::create(window);
+
+        let adapter = wgpu::Adapter::request(
+            &wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::Default,
+                compatible_surface: Some(&surface),
+            },
+            wgpu::BackendBit::PRIMARY,
+        )
+            .await
+            .expect("Failed to request adapter");
+
+
+        let (device, queue) = adapter.request_device(
+            &wgpu::DeviceDescriptor {
+                extensions: wgpu::Extensions {
+                    anisotropic_filtering: false
+                },
+                limits: wgpu::Limits::default(),
+            }
+        ).await;
+
+        let swap_chain_desc = wgpu::SwapChainDescriptor {
+            usage: wgpu::TextureUsage::OUTPUT_ATTACHMENT,
+            format: wgpu::TextureFormat::Bgra8UnormSrgb,
+            width: size.width,
+            height: size.height,
+            present_mode: wgpu::PresentMode::Fifo,
+        };
+
+        let swap_chain = device.create_swap_chain(&surface, &swap_chain_desc);
+
+        State {
+            surface,
+            adapter,
+            device,
+            queue,
+            swap_chain,
+            swap_chain_desc,
+            size,
+        }
+    }
+
+    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        self.size = new_size;
+        self.swap_chain_desc.height = new_size.height;
+        self.swap_chain_desc.width = new_size.width;
+        self.swap_chain = self.device.create_swap_chain(&self.surface, &self.swap_chain_desc);
+
+        // Redrawing after resize
+        self.update();
+        self.render();
+    }
+
+    // input() won't deal with GPU code, so it can be synchronous
+    fn input(&mut self, _: &WindowEvent) -> bool {
+        false
+    }
+
+    fn update(&mut self) {
+
+    }
+
+    fn render(&mut self) {
+        let frame = self.swap_chain
+            .get_next_texture()
+            .expect("Failed to get next frame");
+
+        let mut encoder = self.device
+            .create_command_encoder(
+                &wgpu::CommandEncoderDescriptor {
+                    label: Some("My first command encoder")
+                }
+            );
+
+        encoder.begin_render_pass(
+            &wgpu::RenderPassDescriptor {
+                color_attachments: &[
+                    wgpu::RenderPassColorAttachmentDescriptor {
+                        attachment: &frame.view,
+                        resolve_target: None,
+                        load_op: wgpu::LoadOp::Clear,
+                        store_op: wgpu::StoreOp::Store,
+                        clear_color: wgpu::Color {
+                            r: 1f64,
+                            g: 0.5f64,
+                            b: 1f64,
+                            a: 1f64
+                        }
+                    }
+                ],
+                depth_stencil_attachment: None
+            }
+        );
+
+        self.queue.submit(
+            &[
+                encoder.finish()
+            ]
+        )
+    }
+}
